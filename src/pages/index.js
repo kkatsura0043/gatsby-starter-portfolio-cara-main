5bf051b154473358292e717e4bba3ebb1893923b
username <>
2022-01-13 13:14:39 +0900

    index変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 
-const BlogPage = () => {
+const Portfolio = () => {
     return (
         <div>
-            <h1>Gatsby Blog Site</h1>
+            <h1>Portfolio</h1>
         </div>
     )
 }
 
-export default BlogPage
+export default Portfolio
